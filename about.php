80b07e29c4a23009428ddadf74e590c49688b19c
nhutdh1103 <nhutdh1103@gmail.com>
2022-04-12 01:55:14 -0700

    Update About us

--- about.php
+++ about.php
@@ -43,17 +43,18 @@
 
       <div class="row">
           <div class="col-2">
-              <p>Welcome to Goceries, the online platform for the OFS grocery store in San Jose. We strive to be your number one source for your groceries! 
+              <p>Welcome to FoodiGo, the online platform for the OFS grocery store in Downtown San Jose. We strive to be your number one source for your groceries! 
                 We're dedicated to providing you the very best of our products, with an emphasis on our delivery speed,  your satisfaction, and for society's good.</p>
                 <br>
-              <p>Founded in 2020, We are a small startup focusing on delivering groceries to your doorstep while maintaining the highest quality and freshness of our products.
+              <p>Founded in 2022, We are a small startup focusing on delivering groceries to your doorstep while maintaining the highest quality and freshness of our products.
                 We strive to break up the monopoly by the few grocery store giants by offering extremely competitive prices and best services to you.
               </p>
                 <br>
-              <p>We hope you enjoy our products as much as we enjoy offering them to you! If you have any questions or comments, please don't hesitate to contact us.</p>
+              <p>We hope you enjoy our products with the exceptional services</p> 
+              <p>If you have any questions or comments, please don't hesitate to contact us.</p>
                 <br>
               <p>Sincerely,</p>
-              <p><em>Goceries Team</em></p>
+              <p><em>FoodiGo Team</em></p>
           </div>
 
           <div class ="col-2">
